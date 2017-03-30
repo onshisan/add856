1c0da35a47db945ef5a5b993cb3699a47bf50fa5
Greg Smith <greg.smith@gmail.com>
2017-03-30 00:19:55 -0400

    Update expansion_notes.bash

--- expansion_notes.bash
+++ expansion_notes.bash
@@ -10,11 +10,11 @@ echo "Susbstring: " ${foo:5:3}
 # TEST SET 2
 foo=$(wc -l itemlist.txt)
 echo
-echo "Whole string: " $foo		# echo input
+echo "Whole string: " $foo	    	# echo input
 echo "Length of string: " ${#foo}	#echo length of foo
 # echo `expr substr $foo 1 2`
-foo=$(wc -1 > itemlist.txt)   # see: http://stackoverflow.com/questions/12022319/bash-echo-number-of-lines-of-file-given-in-a-bash-variable-without-the-file-name
-echo "Whole string: " $foo		# echo output
+foo=$(wc -1 > itemlist.txt)       # see: http://stackoverflow.com/questions/12022319/bash-echo-number-of-lines-of-file-given-in-a-bash-variable-without-the-file-name
+echo "Whole string: " $foo		    # echo output
 
 # NOTES
 # echo ${foo:0:2} # extract 2 chars starting at index 0
