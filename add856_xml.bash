41586cce8a9bbfbc59ab0a212656da8cf25fa604
Greg Smith <greg.smith@gmail.com>
2022-05-22 14:26:07 -0400

    Update add856_xml.bash
    Cleanup comments and code that doesn't belong in this version of the script.

--- add856_xml.bash
+++ add856_xml.bash
@@ -4,15 +4,15 @@
 # // In future this script may be enhanced to provide for interactive input of item list filename (currently "itemlist.txt", see line 34) and $z display text string (see line 21).
 # // For now, both parameters are hard-coded; you may edit them below if necessary to suit your application. See comments in code below for more information.
 
-declare -i insertPoint					# declare variable to set insertion point for 856 tag in each file
-declare -i processedItems=0				# declare variable to count and the number of items processed
-# declare -i errorItems=0				# [not implemented] declare variable to count and report the number of items not processed
+declare -i insertPoint					# variable to locate 856 tags in each file
+declare -i processedItems=0				# variable to count and the number of items processed
+declare -i errorItems=0					# variable to count and report the number of items not processed
 
 while IFS='' read -r item || [ -n "$item" ]; do
 	
 	file="${item}_marc.xml"
 	
-	[[ ! -f "$file" ]] && continue		# skip bad items / missing files (add error reporting by incrementing $errorItems here?) 
+	[[ ! -f "$file" ]] && continue		# skip bad items (missing files). Could count errors here by incrementing $errorItems. 
 	
 	insertPoint=$(wc -l < "$file")		# identify location to insert 856 tag based number of lines in the file; trim filename from output of wordcount function and assign to variable
 	
@@ -21,11 +21,6 @@ while IFS='' read -r item || [ -n "$item" ]; do
 	sed -i "$insertPoint i\    <subfield code=\"z\">Full text available at / Plein texte au site : Archive.org</subfield>" "$file"  # append $z subfield. Edit OPAC display text here if desired.
 	sed -i "$insertPoint i\    <subfield code=\"u\">https://archive.org/details/$item</subfield>" "$file" # append $u subfield. Edit base URL here if desired.
 	sed -i "$insertPoint i\  <datafield tag=\"856\" ind1=\"4\" ind2=\"0\">" "$file" # append the opening XML tag for the 856 field. Edit indicators here if desired.
-	
-	# sed -i "$insertPoint i\Put me in line $insertPoint, please" "$file" # example code for text insertion before </record> tag
-	
-	# // single-line echo code for .mrk/.mrc files; not implemented in MARCXML version
-	# echo "=856  40\$uhttps://archive.org/details/$item\$zFull text available at / Plein texte au site : Archive.org" >> "$file"   # append the identifier to the file (add code to insert user input here later; $z subfield hard-coded for now)
 	
 	echo -e "For identifier '\033[37;44m$item\033[0m' : File is $insertPoint lines : 856 tag appended to '\033[37;41m$file\033[0m'" # show progress via console
 	
