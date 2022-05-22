c27f007f5ce1e93b1efe643a38d8af7216e4bdb1
Greg Smith <greg.smith@gmail.com>
2022-05-22 14:28:22 -0400

    Update expansion_notes.bash
    Cleanup

--- expansion_notes.bash
+++ expansion_notes.bash
@@ -4,7 +4,7 @@
 declare foo="superstring"
 echo
 echo "Whole string: " $foo
-echo "Length of strong: " ${#foo}
+echo "Length of string: " ${#foo}
 echo "Susbstring: " ${foo:5:3}
 
 # TEST SET 2
@@ -13,6 +13,7 @@ echo
 echo "Whole string: " $foo	    	# echo input
 echo "Length of string: " ${#foo}	#echo length of foo
 # echo `expr substr $foo 1 2`
+
 foo=$(wc -1 > itemlist.txt)       # see: http://stackoverflow.com/questions/12022319/bash-echo-number-of-lines-of-file-given-in-a-bash-variable-without-the-file-name
 echo "Whole string: " $foo		    # echo output
 
